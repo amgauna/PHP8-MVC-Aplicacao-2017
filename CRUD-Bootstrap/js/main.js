a0d5c31374cc998d69403c828f3271ab245c7d13
Ana Mercedes Gauna <amgauna@gmail.com>
2017-07-28 12:59:26 -0300

    Add files via upload

---
+++ CRUD-Bootstrap/js/main.js
@@ -0,0 +1,12 @@
+/**
+ * Passa os dados do cliente para o Modal, e atualiza o link para exclusão
+ */
+$('#delete-modal').on('show.bs.modal', function (event) {
+  
+  var button = $(event.relatedTarget);
+  var id = button.data('customer');
+  
+  var modal = $(this);
+  modal.find('.modal-title').text('Excluir Cliente #' + id);
+  modal.find('#confirm').attr('href', 'delete.php?id=' + id);
+})
