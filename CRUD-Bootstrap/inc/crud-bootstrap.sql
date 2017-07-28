cbfd4b49bbbe9ff8eed20436ad2bd42901747a88
Ana Mercedes Gauna <amgauna@gmail.com>
2017-07-28 13:01:38 -0300

    Add files via upload

---
+++ CRUD-Bootstrap/inc/crud-bootstrap.sql
@@ -0,0 +1,22 @@
+CREATE TABLE customers (
+  id int(11) NOT NULL,
+  name varchar(255) NOT NULL,
+  cpf_cnpj varchar(14) NOT NULL,
+  birthdate date NOT NULL,
+  address varchar(255) NOT NULL,
+  hood varchar(100) NOT NULL,
+  zip_code int(8) NOT NULL,
+  city varchar(100) NOT NULL,
+  state varchar(100) NOT NULL,
+  phone int(13) NOT NULL,
+  mobile int(13) NOT NULL,
+  ie int(11) NOT NULL,
+  created datetime NOT NULL,
+  modified datetime NOT NULL
+);
+
+ALTER TABLE customers
+  ADD PRIMARY KEY (id);
+  
+ALTER TABLE customers
+  MODIFY id int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
