cbb37184f021231662933d525c9668405836996d
Ana Gauna <amgauna@gmail.com>
2021-10-28 20:19:47 -0300

    Delete SistemaCadastro-PDO-CRUD directory

--- SistemaCadastro-PDO-CRUD/cria-usuarios.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE users(
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT, -- id
-    name VARCHAR(60) NOT NULL, -- nome
-    email VARCHAR(80) NOT NULL, -- email
-    gender ENUM('m', 'f') NOT NULL, -- gênero (masculino, feminino)
-    birthdate DATE NOT NULL, -- data de nascimento
-    PRIMARY KEY(id)
-) COLLATE=utf8_unicode_ci;
